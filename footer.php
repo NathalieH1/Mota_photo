7100c67ff94003dcfd1620dd187c98ccb29f344a
Hnat <nathalie-humbert@outlook.com>
2024-04-18 12:19:04 +0200

    Mise à jour avec appel de la modale

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
-<footer>
-
-<nav>
-    <?php
-    wp_nav_menu(array(
-        'theme_location' => 'footer-menu',
-        'container' => false,
-    ));
-    ?>
-</nav>
-<?php get_template_part('templates-parts/modale_contact'); ?>
-
-    </footer>
-    <?php wp_footer(); ?> 
+</main>
+<footer class="footer">
+    <nav class="footer_nav">
+        <?php
+            wp_nav_menu(array(
+                'theme_location' => 'footer-menu',
+                'container' => false,
+            ));
+        ?>
+        <ul>
+            <li>Tous droits réservés</li>
+        </ul>
+    </nav>
+    <?php get_template_part('Templates-parts/modale_contact'); ?>
+    <?php wp_footer(); ?>
+</footer>
 </body>
 </html>
