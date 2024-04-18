3037a8a250cc8c67caed185d3fc33492a632aa20
Hnat <nathalie-humbert@outlook.com>
2024-04-18 12:29:16 +0200

    Mise en place de la fonction de navigation des photos

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,82 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var li = document.getElementById("menu-item-69");
-
-// Get the link inside the button
-var link = li.querySelector('a');
-
-// When the user clicks on the link, open the modal
-li.addEventListener('click', function(event) {
-    event.preventDefault(); // Empêcher le comportement par défaut du lien
-    modal.style.display = "block"; // Ouvrir la modale
-});
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+(function ($) {
+    'use strict';
+  
+  let pageActuelle = 1;
+  
+  $('#btn-charger_plus').on('click', function () {
+      pageActuelle++;
+      ajaxRequest(true);
+  });
+  
+  $(document).on('change', '.taxonomie', function (e) {
+      e.preventDefault();
+      pageActuelle = 1;
+      ajaxRequest(false);
+  });
+  
+     
+  function ajaxRequest(chargerPlus) {
+      var categorieSelection = $('#select-categorie').val();
+      var formatSelection = $('#select-format').val();
+      var ordre = $('#select-ordre').val();
+  
+      $.ajax({
+          type: 'POST',
+          url: my_ajax_obj.ajax_url,
+          dataType: 'html',
+          data: {
+              action: 'filter',
+              categorieTaxonomie: 'categorie',
+              categorieSelection: categorieSelection,
+              formatTaxonomie: 'format',
+              formatSelection: formatSelection,
+              orderDirection: ordre,
+              page: pageActuelle,
+          },
+          success: function (resultat) {
+              if (chargerPlus) {
+                  $('.photo_type').append(resultat);
+              } else {
+                  $('.photo_type').html(resultat);
+              }
+  
+              if (categorieSelection === 'Mariage' && pageActuelle >= 3) {
+                  $('#charger_plus_btn').attr('style', 'display: none;');
+              } else if (pageActuelle === 5) {
+                  $('#charger_plus_btn').attr('style', 'display: none;');
+              } else if (
+                  (categorieSelection === 'Concert' || categorieSelection === 'Reception' 
+                  || categorieSelection === 'Television') && pageActuelle === 1) {
+                  $('#charger_plus_btn').attr('style', 'display: none;');
+              } else {
+                  $('#charger_plus_btn').attr('style', 'display: block;');
+              }
+          },
+          error: function (result) {
+              console.warn(result);
+          },
+      });
+  }
+  
+  
+  
+  navigationPhotos($('.arrow-gauche'), $('.previous-image'));
+  navigationPhotos($('.arrow-droite'), $('.next-image'));
+  
+    function navigationPhotos(arrow, image) {
+      arrow.hover(
+        function () {
+          image.css('opacity', '1');
+        },
+        function () {
+          image.css('opacity', '0');
+        }
+      );
     }
-}
+  
+  
+  })(jQuery);  
+  
 
 
 
