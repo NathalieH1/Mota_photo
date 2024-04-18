5af4f646beed93aaff3335bef2a5297b3ca49d90
Hnat <nathalie-humbert@outlook.com>
2024-04-18 12:21:40 +0200

    Mise à jour de de la fonction wp_nav_menu

--- header.php
+++ header.php
@@ -7,20 +7,23 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-    
-    <?php wp_body_open(); ?>
-<header>
+<body >
 
-<nav id="nav_header" role="navigation" aria-label="<?php _e('Menu principal', 'Mota_photo'); ?>">
-    <a href="<?php echo home_url( '/' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/Photos NMota/Logo.png" alt="Logo NMota" id="logo"></a>
-    <?php
-        wp_nav_menu([
-            'theme_location' => 'main-menu',
-            'container'      => false, // On retire le conteneur généré par WP
-            'walker'         => new A11y_Walker_Nav_Menu()
-        ]);
-    ?>
-</nav>
+<header>
+    <section class="header">
+        <div>
+          <a href="<?php echo home_url( '/' ); ?>"><img class="header_logo" src="<?php echo get_template_directory_uri(); ?>/Photos NMota/Logo.png" alt="Logo NMota" id="logo"></a>
+        </div>
+        
+        <nav id="nav_header" role="navigation" aria-label="<?php _e('Menu principal', 'Mota_photo'); ?>">
+            
+            <?php
+                wp_nav_menu(['theme_location' => 'main-menu']);
+            ?>
+        </nav>
+
+    </section>
+
     
 </header>
+<main>
