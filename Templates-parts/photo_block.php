fcc0199830f23f2c7f997045d6b1c3e73f08ae0a
Hnat <nathalie-humbert@outlook.com>
2024-05-03 14:52:21 +0200

    Modification sens photo précédente et photo suivante

--- Templates-parts/photo_block.php
+++ Templates-parts/photo_block.php
@@ -1,11 +1,43 @@
+
 <!--boucle WP en cours pour afficher chaque photo de la liste-->
-<?php $photos->the_post(); ?>
-  <!--conteneur divisé en 2 parties-->
+<?php $photos->the_post();global $post;
+$current_post = $post;
+$next_post = get_adjacent_post(false, '', false);
+if ($next_post) {
+    // Récupérer les informations nécessaires du post suivant
+    $next_post_ref = get_field('reference', $next_post->ID);
+    $next_post_categorie = strip_tags(get_the_term_list($next_post->ID, 'categorie'));
+    $next_post_img = get_the_post_thumbnail_url($next_post);
+} else {
+    $next_post_ref = 'null';
+    $next_post_categorie = 'null';
+    $next_post_img = 'null';
+}$previous_post = get_adjacent_post(false, '', true);// Vérifier si le post précédent existe
+if ($previous_post) {
+    // Récupérer les informations nécessaires du post précédent
+    $previous_post_ref = get_field('reference', $previous_post->ID);
+    $previous_post_categorie = strip_tags(get_the_term_list($previous_post->ID, 'categorie'));
+    $previous_post_img = get_the_post_thumbnail_url($previous_post);
+} else {
+    $previous_post_ref = 'null';
+    $previous_post_categorie = 'null';
+    $previous_post_img = 'null';
+}?>
+  <!--conteneur divisé en 2 parties-->  
   <div class="half">
   <!--conteneur principal de chaque photo-->
       <div class="post_photo">
           <!--cette ligne affiche l'image mise en avant de l'article actuel en utilisant la fonction "the_post_thumbnail_url()"-->
-          <img class="img-photo" src="<?php echo the_post_thumbnail_url(); ?>" />
+          <img
+           data-next-img="<?php echo $next_post_img; ?>"
+           data-next-ref="<?php echo $next_post_ref; ?>"
+           data-next-cat="<?php echo $next_post_categorie; ?>"
+           data-previous-img="<?php echo $previous_post_img; ?>"
+           data-previous-ref="<?php echo $previous_post_ref; ?>"
+           data-previous-cat="<?php echo $previous_post_categorie; ?>"
+           data-ref="<?php echo get_field('reference'); ?>"
+           data-categorie="<?php echo strip_tags(get_the_term_list($photos->ID, 'categorie')); ?>"
+           class="img-photo" src="<?php echo the_post_thumbnail_url(); ?>" />
           <div>
               <div class="photo-hover">
                 <!--cette ligne affiche une icône pour passer en mode plein écran-->
@@ -24,4 +56,4 @@
               </div>
           </div>
       </div>
-  </div> 
+  </div>
