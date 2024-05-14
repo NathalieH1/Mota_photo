bd05b849bf1cb982fdcddf24fc164f1175c41597
Hnat <nathalie-humbert@outlook.com>
2024-05-14 22:46:38 +0200

    Ajout fichier de la lightbox

---
+++ Templates-parts/lightbox.php
@@ -0,0 +1,13 @@
+<!--Lightbox-->
+
+<div class="lightbox hidden" id="lightbox">
+      <button class="lightbox__close" title="Refermer cet agrandissement"></button>
+      <div class="lightbox__container">
+          <div class="lightbox__loader hidden"></div>
+          <div class="lightbox__container_info flexcolumn" id="lightbox__container_info">
+              <div class="lightbox__container_content flexcolumn" id="lightbox__container_content"></div>
+              <button class="lightbox__next" aria-label="Voir la photo suivante" title="Photo suivante"></button>
+              <button class="lightbox__prev" aria-label="Voir la photo précente" title="Photo précédente"></button>
+          </div>
+      </div>
+  </div>
