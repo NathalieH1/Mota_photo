7fecbce93272555a0264dd1be16fc4dc2ca20b20
Hnat <nathalie-humbert@outlook.com>
2024-05-14 22:45:04 +0200

    Réajustement miniature image précédente et suivante

--- single.php
+++ single.php
@@ -13,7 +13,7 @@
 
 <!-- Section de la page dédiée à l'affichage des détails d'une photo unique -->
  <div class="single-page">
-
+ <!-- Affichage des infos du post courant -->
    <section class="single-photo organisation">
 	 <div class="single-description half">
 	   <h1><?php the_title() ?></h1>
@@ -35,14 +35,23 @@
 
 	<!-- Navigation entre les photos et affichage des photos en miniature --> 
 	 <div class="interaction-photo__navigation">
+		<!-- Récupération du post suivant et précédent pour afficher leur image au-dessus des flèches -->
 	   <?php
 					 $prevPost = get_previous_post();
 					 $nextPost = get_next_post();
+					 if (!empty($prevPost)) {
+						$prevThumbnail = get_the_post_thumbnail_url( $prevPost->ID );
+						$prevLink = get_permalink($prevPost); 
+					}	
+					if (!empty($nextPost)) {
+						$nextThumbnail = get_the_post_thumbnail_url( $nextPost->ID );
+						$nextLink = get_permalink($nextPost); 
+					}	
 				 ?>
 	   <div class="arrows">
+		<!-- affichage  de la flèche pour le post précédent contenant son url -->
 		 <?php if (!empty($prevPost)) {
-						 $prevThumbnail = get_the_post_thumbnail_url( $prevPost->ID );
-						 $prevLink = get_permalink($prevPost); ?>
+					 ?>
 		 <a href="<?php echo $prevLink; ?>">
 		   <img class="arrow arrow-gauche" src="<?php echo get_template_directory_uri(); ?>/Photos NMota/arrow_left.png"
 			 alt="Flèche pointant vers la gauche" />
@@ -50,9 +59,9 @@
 		 <?php } else { ?>
 		 <img style="opacity:0; cursor: auto;" class="arrow "
 		   src="<?php echo get_template_directory_uri(); ?>/Photos NMota/arrow_left.png" />
+		  	<!-- affichage  de la flèche pour le post suivant contenant son url --> 
 		 <?php } if (!empty($nextPost)) {
-						 $nextThumbnail = get_the_post_thumbnail_url( $nextPost->ID );
-						 $nextLink = get_permalink($nextPost); ?>
+						 ?>
 		 <a href="<?php echo $nextLink; ?>">
 		   <img class="arrow arrow-droite"
 			 src="<?php echo get_template_directory_uri(); ?>/Photos NMota/arrow_right.png"
@@ -60,11 +69,13 @@
 		 </a>
 		 <?php } ?>
 	   </div>
+	   <!-- image du post précédent -->
 	   <div class="preview">
-		 <img class="previous-image" src="<?php echo $prevThumbnail; ?>" alt="Prévisualisation image précédente">
+		 <img class="previous-image" src="<?php echo $nextThumbnail; ?>" alt="Prévisualisation image précédente">
 	   </div>
+	    <!-- image du post suivant -->
 	   <div class="preview">
-		 <img class="next-image" src="<?php echo $nextThumbnail; ?>" alt="Prévisualisation image suivante">
+		 <img class="next-image" src="<?php echo $prevThumbnail; ?>" alt="Prévisualisation image suivante">
 	   </div>
 	 </div>
    </section>
@@ -73,6 +84,7 @@
    <section class="recommandations">
 	 <h2>Vous aimerez aussi</h2>
 	 <div class="recommandations__images organisation">
+		<!-- récupération des posts qui ont la même catégorie que le post courant -->
 	   <?php
 				 $categorie = strip_tags(get_the_term_list($post->ID, 'categorie'));
 				 $photos = new WP_Query(array (
@@ -95,14 +107,11 @@
 							 include 'Templates-parts/photo_block.php';
 						 }
 					 } else {
-						 echo '';
+						 echo '<p class="texte">Il n\'y a pas encore d\'autres photos à afficher dans cette catégorie.</p>';
 					 }
-					 wp_reset_postdata();
 					 
 				 }
-				 else {
-					 echo '<p class="texte">Il n\'y a pas encore d\'autres photos à afficher dans cette catégorie.</p>';
-				 }
+				 wp_reset_postdata();
 				 
 			 ?>
  
