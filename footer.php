91a40cf34fc386e07cdbfb8e6745423dda5bf615
Hnat <nathalie-humbert@outlook.com>
2024-05-24 12:43:59 +0200

    Modification du menu en liens et mention texte

--- footer.php
+++ footer.php
@@ -1,17 +1,20 @@
 </main>
-<?php get_template_part('Templates-parts/modale_contact'); ?>
+<?php get_template_part('templates-parts/modale_contact'); ?>
 <?php get_template_part('templates-parts/lightbox'); ?>
 <footer class="footer">
     <nav class="footer_nav">
-        <?php
-            wp_nav_menu(array(
-                'theme_location' => 'footer-menu',
-                'container' => false,
-            ));
-        ?>
+    <div class="footer-links">
+            <a class="lien-footer" href="<?php echo esc_url( home_url( '/mentions-legales/' ) ); ?>">Mentions légales</a>
+            <a id="lien-footer2" class="lien-footer" href="<?php echo esc_url( home_url( '/politique-de-confidentialite/' ) ); ?>">Vie privée</a>
+            <a id="mention-texte">TOUS DROITS RÉSERVÉS</a>
+        
+</div>
+       
     </nav>
     
     <?php wp_footer(); ?>
+
+
 </footer>
 </body>
 </html>
