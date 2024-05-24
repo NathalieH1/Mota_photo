7958889efbdbe088cd6aa8ffb9974e64fb3a1931
Hnat <nathalie-humbert@outlook.com>
2024-05-24 13:07:48 +0200

    Ajout code pour renvoyer à la page du RGPD de WordPress

--- page.php
+++ page.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
-
+<?php
+while ( have_posts() ) :
+    the_post();
+?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header><!-- .entry-header -->        <div class="entry-content">
+            <?php the_content(); ?>
+        </div><!-- .entry-content -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+<?php
+endwhile; // End of the loop.
+?>
 
 
 <?php get_footer(); ?>
